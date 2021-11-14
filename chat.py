a765310cba9a3850c8e6e9d53ac048e15a40d96d
nnayk <79224511+nnayk@users.noreply.github.com>
2021-11-14 15:27:38 -0800

    ratemprofs working

--- chat.py
+++ chat.py
@@ -1,49 +1,58 @@
-entered = input("enter: ")
+def getRatings(entered):
+    with open('ratemyprofs.txt','r') as f:
+        lines = f.readlines()
 
-with open('ratemyprofs.txt','r') as f:
-    lines = f.readlines()
+    tID=""
+    lastNames=[]
+    overallRating=""
+    for line in lines:
+        rmpTeacherDict = eval(line)
+        tLastName = rmpTeacherDict['tLname']
+        lastNames.append(tLastName)
+        #print(f"lastnames={lastNames}")
+        if entered.lower() == rmpTeacherDict['tLname'].lower():
+            if tID=="":
+                tID = str(rmpTeacherDict['tid'])
+                overallRating = rmpTeacherDict['overall_rating']
+            #print(f"id={tID}")
+            #print(f"{entered} id = {tID}")
 
-tID=[]
-for line in lines:
-    rmpTeacherDict = eval(line)
-    tLastName = rmpTeacherDict['tLname']
-    if entered.lower() == rmpTeacherDict['tLname'].lower():
-        tID.append(str(rmpTeacherDict['tid']))
-        print(f"{entered} id = {tID}")
+    with open('ratereviews2.txt','r') as f:
+        lines = f.readlines()
 
-with open('ratereviews.txt','r') as f:
-    lines = f.readlines()
+    readIn=False
+    done=False
+    rating=[]
 
-readIn=False
-rating=[]
+    TOKEN="***{{{"
 
-TOKEN="***{{{"
-counter=len(tID)
-print(f"hehe={tID},counter={counter}")
-for line in lines[2:]:
-    #print(line)
-    #print(f"{line}")
-    if line.strip('\n') == TOKEN and counter==0:
-        print("TOKEN")
-        break
-    if line.strip('\n') in tID:
-        print("started")
-        print(f"tid={tID}")
-        counter-=1
-        print(f"county={counter}")
-        readIn = True
-    if readIn:
-        rating.append(line.split(','))
+    #print(f"hehe={tID},counter={counter}")
+    for line in lines[2:]:
+        #print(line)
+        #print(f"{line}")
+        if line.strip('\n') == TOKEN and done:
+            #print("TOKEN")
+            break
+        if line.strip('\n') in tID:
+            #print("started")
+            #print(f"tid={tID}")
+            #print(f"county={counter}")
+            done=True
+            readIn = True
+        if readIn:
+            #print(f"splitty={line.split(',')}")
+            rating.append(line.split(','))
 
-print(f"class={rating}")
+    rating=rating[1:]
+    print("rating=",rating)
 
+    #print(f"rating={rating[0][10]}")
 
+    feedback=[]
+    for clas in rating:
+        feedback.append({"class":clas[10],"attendance":clas[0],"clarity":clas[1],"difficulty":clas[2],"message":clas[11],"score":overallRating})
+    print(feedback)
+    return feedback
 
-
-#for line in lines[0:1]:
-
-
-
-
-
+getRatings("abney")
 
